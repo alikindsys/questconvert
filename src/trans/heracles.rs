b68cc3e04b15c96c8c752701086018026359248b
alikindsys <alice@blocovermelho.org>
2026-04-23 01:50:12 -0300

    Implement partial migration of tasks

--- src/trans/heracles.rs
+++ src/trans/heracles.rs
@@ -7,6 +7,15 @@ use crate::formats::{
     heracles,
 };
 
+pub trait TryFromWithId<T>: Sized {
+    type Error;
+    fn try_from_with_id(value: T, id: String) -> Result<Self, Self::Error>;
+}
+
+pub trait FromWithId<T>: Sized {
+    fn from_with_id(value: T, id: String) -> Self;
+}
+
 impl ftbquests::State {
     pub fn from_heracles(state: heracles::State) -> Self {
         let _page_map: HashMap<String, HashSet<ftbquests::Quest>> = HashMap::new();
@@ -60,5 +69,91 @@ impl ftbquests::State {
         }
 
         unimplemented!()
+    }
+}
+
+impl FromWithId<heracles::TaskRef> for ftbquests::TaskRef {
+    fn from_with_id(value: heracles::TaskRef, id: String) -> Self {
+        match value {
+            heracles::TaskRef::Check { icon, .. } => {
+                Self::Checkmark { id, icon: icon.map(|it| it.into()) }
+            }
+            heracles::TaskRef::Item { item, amount, icon, .. } => {
+                let fitem: ftbquests::Item;
+                if let Some(amount) = amount {
+                    fitem = ftbquests::Item::Compound(ftbquests::ItemRef {
+                        count: Some(amount),
+                        id: item,
+                        tag: None,
+                    });
+                } else {
+                    fitem = ftbquests::Item::Simple(item);
+                }
+
+                Self::Item { id, item: fitem, icon: icon.map(|it| it.into()) }
+            }
+            // Fuck my life. Pre-processing these.
+            heracles::TaskRef::Advancement { advancements, icon, .. } => {
+                assert!(
+                    advancements.len() == 1,
+                    "Heracles' advancement task contained more then one advancement.
+                    These should be pre-processed before submission to FromWithId."
+                );
+                Self::Advancement {
+                    id,
+                    advancement: advancements.first().unwrap().clone(),
+                    criterion: None,
+                    icon: icon.map(|it| it.into()),
+                }
+            }
+            heracles::TaskRef::Structure { structures, icon, .. } => Self::Structure {
+                id,
+                structure: structures,
+                icon: icon.map(|it| it.into()),
+            },
+            heracles::TaskRef::ChangeDimension { to, icon, .. } => {
+                Self::Dimension { id, dimension: to, icon: icon.map(|it| it.into()) }
+            }
+            heracles::TaskRef::ItemInteraction { item, icon, .. } => Self::Item {
+                id,
+                item: ftbquests::Item::Simple(item),
+                icon: icon.map(|it| it.into()),
+            },
+            heracles::TaskRef::BlockInteraction { block, icon, .. } => Self::Observation {
+                id,
+                observe_type: ftbquests::ObserveType::Block,
+                to_observe: block,
+                icon: icon.map(|it| it.into()),
+            },
+            heracles::TaskRef::KillEntity { entity, amount, icon, .. } => Self::Kill {
+                id,
+                entity: entity._type,
+                value: amount,
+                icon: icon.map(|it| it.into()),
+            },
+            heracles::TaskRef::Biome { biomes, icon, .. } => {
+                Self::Biome { id, biome: biomes, icon: icon.map(|it| it.into()) }
+            }
+            heracles::TaskRef::Composite { tasks, amount, title, icon } => todo!(),
+        }
+    }
+}
+
+impl From<heracles::Icon> for ftbquests::Item {
+    fn from(value: heracles::Icon) -> Self {
+        value.item.into()
+    }
+}
+
+impl From<heracles::TagItemRef> for ftbquests::Item {
+    fn from(value: heracles::TagItemRef) -> Self {
+        match value {
+            heracles::TagItemRef::TagRef(s) => Self::Simple(s),
+            heracles::TagItemRef::ItemRef(item_ref) => Self::Compound(ftbquests::ItemRef {
+                count: item_ref.count,
+                id: item_ref.id,
+                tag: None,
+            }),
+        }
     }
 }
